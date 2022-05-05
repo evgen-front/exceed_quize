cc3c16622d5e4602ca0cdb2d4c8b2c2c0d533b21
evgen-front <ebondarenko.exceedteam@gmail.com>
2022-05-05 13:59:48 +0300

    separate question block

--- src/Pages/NewTest/AddQuestion/AddQuestion.tsx
+++ src/Pages/NewTest/AddQuestion/AddQuestion.tsx
@@ -5,29 +5,44 @@ import {
   EditTwoTone,
   PlusOutlined,
 } from "@ant-design/icons";
-import { Button, Checkbox, Input } from "antd";
-import { Dispatch, FC, SetStateAction, useEffect, useState } from "react";
-import { NewTestService } from "../../../services/NewTestService";
-import { Answer, Question, QuestionResponse } from "../../../types/types";
+import { Button, Input, Modal } from "antd";
+import { useEffect, useState } from "react";
+import { NewTestService } from "../../../services/NewTestService"; // !!! how can I to reduce a pass?
+import { Question, QuestionResponse } from "../../../types/types";
 import { AddAnswer } from "../AddAnswer/AddAnswer";
 import "./AddQuestion.scss";
 
-interface AddQuestionProps {
-  questionId: number | null;
-  testId: number | null;
-  listQuestion: QuestionResponse[];
-  setQuestionId: Dispatch<SetStateAction<number | null>>;
-}
-
-export const AddQuestion: FC<AddQuestionProps> = ({
-  questionId,
-  testId,
-  listQuestion,
-  setQuestionId,
-}) => {
+export const AddQuestion = ({ testId }: { testId: number | null }) => {
+  const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
+  const [questionList, setQuestionList] = useState<QuestionResponse[]>([]);
+  const [questionId, setQuestionId] = useState<number | null>(null);
   const [questionTitle, setQuestionTitle] = useState<string>("");
   const [editQuestionFlag, setEditQuestionFlag] = useState<boolean>(false);
   const [editableQuestion, setEditableQuestion] = useState<Question | {}>({});
+
+  const openModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const openEditModal = (id: number, text: string) => {
+    openModal();
+    setQuestionId(id);
+    setQuestionTitle(text);
+  };
+
+  const handleModalOK = () => {
+    setIsModalVisible(false);
+    setQuestionId(null);
+    setQuestionTitle("");
+    setEditQuestionFlag(false);
+  };
+
+  const handleModalCancel = () => {
+    setIsModalVisible(false);
+    setQuestionId(null);
+    setQuestionTitle("");
+    setEditQuestionFlag(false);
+  };
 
   const handleQuestionTitle = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setQuestionTitle(e.target.value);
@@ -55,9 +70,18 @@ export const AddQuestion: FC<AddQuestionProps> = ({
     setEditQuestionFlag(false);
   };
 
+  const deleteQuestion = (id: number) => {
+    NewTestService.deleteQuestion(testId, id)
+      .then((res) => {
+        setQuestionList([...questionList].filter((q) => q.id !== id));
+        setQuestionId(null);
+      })
+      .catch((err) => console.log(err.message));
+  };
+
   const startEditQuestion = () => {
     setEditQuestionFlag(true);
-    const editQuestion = listQuestion.filter(
+    const editQuestion = questionList.filter(
       (elem) => elem.id === questionId
     )[0];
     setEditableQuestion(editQuestion);
@@ -69,63 +93,110 @@ export const AddQuestion: FC<AddQuestionProps> = ({
     setEditQuestionFlag(false);
   };
 
+  useEffect(() => {
+    if (testId) {
+      NewTestService.getQuestions(testId)
+        .then((res) => setQuestionList(res.data))
+        .catch((err) => console.log(err)); //!!!
+    }
+  }, [questionId, testId]);
+
   return (
-    <div className="NTModalWrapper">
-      <p>Создать новый вопрос...</p>
-      {!questionId ? (
-        <div className="NTModal_Question">
-          <Input.TextArea
-            placeholder="Введите текст вопроса"
-            name="question_title"
-            onChange={handleQuestionTitle}
-            value={questionTitle}
-          />
-          <Button
-            className="NTSaveButton"
-            type="primary"
-            shape="round"
-            size={"middle"}
-            onClick={createNewQuestion}
-          >
-            Далее
-          </Button>
-        </div>
-      ) : (
-        <div className="NTModal_Question">
-          {editQuestionFlag ? (
-            <div className="NTModal_Question_EditBlock">
-              <p className="NTModalQuestionEditBlock_TestName">
-                Редактировать вопрос:
-              </p>
-              <div className="inputButtonWrap">
-                <Input.TextArea
-                  name="editQuestionName"
-                  value={questionTitle}
-                  onChange={handleQuestionTitle}
-                />
+    <div className="questionWrap">
+      <div className="question_viewBlock">
+        <p className="question_viewBlock_title">Вопросы:</p>
+        <div className="question_viewBlock_items">
+          {questionList.length ? (
+            questionList.map(({ id, ordering, text }) => (
+              <div
+                key={`questionItem_${id}`}
+                className="NTQuestionBlock_Items_Item"
+              >
+                <p>
+                  {ordering}. {text}
+                </p>
                 <div className="NT_IconBlock">
-                  <CheckOutlined onClick={updateQuestion} />
-                  <CloseOutlined onClick={undoEditQuestion} />
+                  <EditTwoTone onClick={() => id && openEditModal(id, text)} />
+                  <DeleteTwoTone onClick={() => id && deleteQuestion(id)} />
                 </div>
               </div>
+            ))
+          ) : (
+            <p>В этом тесте ещё нет ни одного вопроса</p>
+          )}
+          <Button
+            className="NTButton_add"
+            type="primary"
+            shape="round"
+            icon={<PlusOutlined />}
+            size={"middle"}
+            onClick={openModal}
+          >
+            Добавить вопрос
+          </Button>
+        </div>
+      </div>
+      <Modal
+        visible={isModalVisible}
+        onOk={handleModalOK}
+        onCancel={handleModalCancel}
+      >
+        <div className="NTModalWrapper">
+          {!questionId ? (
+            <div className="NTModal_Question">
+              <p>Создать новый вопрос...</p>
+              <Input.TextArea
+                placeholder="Введите текст вопроса"
+                name="question_title"
+                onChange={handleQuestionTitle}
+                value={questionTitle}
+              />
+              <Button
+                className="NTSaveButton"
+                type="primary"
+                shape="round"
+                size={"middle"}
+                onClick={createNewQuestion}
+              >
+                Далее
+              </Button>
             </div>
           ) : (
-            <div className="NTModal_Question_Block">
-              <p className="NTModal_Question_Block_TestName">
-                Вопрос: {questionTitle}
-              </p>
-              <div className="NT_IconBlock">
-                <EditTwoTone onClick={startEditQuestion} />
+            <div className="NTModal_Question">
+              <p>Редактировать вопрос...</p>
+              {editQuestionFlag ? (
+                <div className="NTModal_Question_EditBlock">
+                  <div className="inputButtonWrap">
+                    <Input.TextArea
+                      name="editQuestionName"
+                      value={questionTitle}
+                      onChange={handleQuestionTitle}
+                    />
+                    <div className="NT_IconBlock">
+                      <CheckOutlined onClick={updateQuestion} />
+                      <CloseOutlined onClick={undoEditQuestion} />
+                    </div>
+                  </div>
+                </div>
+              ) : (
+                <div className="NTModal_Question_Block">
+                  <p className="NTModal_Question_Block_TestName">
+                    Вопрос: {questionTitle}
+                  </p>
+                  <div className="NT_IconBlock">
+                    <EditTwoTone onClick={startEditQuestion} />
+                  </div>
+                </div>
+              )}
+              <p>Добавьте изображение для вопроса</p>
+              <div style={{ border: "1px solid red", paddingBottom: "10px" }}>
+                здесь будет загрузка изображения
               </div>
+              <AddAnswer questionId={questionId} />
             </div>
           )}
-          <p>Добавьте изображение для вопроса</p>
-          <div style={{ border: "1px solid red", paddingBottom: "10px" }}>
-            здесь будет загрузка изображения
-          </div>
-          <AddAnswer questionId={questionId}/>
         </div>
-      )}
+      </Modal>
     </div>
   );
 };
--- src/Pages/NewTest/NewTest.tsx
+++ src/Pages/NewTest/NewTest.tsx
@@ -1,45 +1,17 @@
-import {
-  CheckOutlined,
-  CloseOutlined,
-  DeleteTwoTone,
-  EditTwoTone,
-  PlusOutlined,
-} from "@ant-design/icons";
-import { Button, Checkbox, Input, Modal } from "antd";
+
+import { Button, Checkbox, Input } from "antd";
 import { CheckboxChangeEvent } from "antd/lib/checkbox";
-import React, { useEffect, useState } from "react";
-import { Main } from "../../Layouts/MainView/Main";
+import React, { useState } from "react";
+import { Main } from "../../Layouts/MainView/Main"; // !!! how can I to reduce a pass?
 import { NewTestService } from "../../services/NewTestService";
-import { Answer, Question, QuestionResponse, Test } from "../../types/types";
-import { AddAnswer } from "./AddAnswer/AddAnswer";
+import { Test } from "../../types/types";
 import { AddQuestion } from "./AddQuestion/AddQuestion";
 import "./NewTest.scss";
 
 export const NewTest = () => {
-  const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
   const [testId, setTestId] = useState<number | null>(null);
   const [testName, setTestName] = useState<string>("");
   const [testPublished, setTestPublished] = useState<boolean>(false);
-  const [questionId, setQuestionId] = useState<number | null>(null);
-  const [listQuestion, setListQuestion] = useState<QuestionResponse[] | []>([]);
-
-  const openModal = () => {
-    setIsModalVisible(true);
-  };
-
-  const handleModalOK = () => {
-    setIsModalVisible(false);
-    setQuestionId(null);
-    // setQuestionTitle("");
-    // setEditQuestionFlag(false);
-  };
-
-  const handleModalCancel = () => {
-    setIsModalVisible(false);
-    setQuestionId(null);
-    // setQuestionTitle("");
-    // setEditQuestionFlag(false);
-  };
 
   const handleTestName = (e: React.ChangeEvent<HTMLInputElement>) => {
     setTestName(e.target.value);
@@ -47,12 +19,6 @@ export const NewTest = () => {
 
   const handleTestPublic = (e: CheckboxChangeEvent) => {
     setTestPublished(e.target.checked);
-  };
-
-  const openEditModal = (id: number, text: string) => {
-    openModal();
-    setQuestionId(id);
-    // setQuestionTitle(text);
   };
 
   const createNewTest = () => {
@@ -65,25 +31,6 @@ export const NewTest = () => {
       .then((res) => setTestId(res.data.id))
       .catch((err) => console.log(err)); //!!!
   };
-
-  const deleteQuestion = (id: number) => {
-    NewTestService.deleteQuestion(testId, id)
-      .then((res) => {
-        setListQuestion([...listQuestion].filter((q) => q.id !== id));
-        setQuestionId(null);
-      })
-      .catch((err) => console.log(err.message));
-  };
-
-  useEffect(() => {
-    console.log(questionId, "questionId");
-
-    if (testId) {
-      NewTestService.getQuestions(testId)
-        .then((res) => setListQuestion(res.data))
-        .catch((err) => console.log(err)); //!!!
-    }
-  }, [questionId, testId]);
 
   return (
     <Main>
@@ -117,56 +64,12 @@ export const NewTest = () => {
             </Button>
           </div>
         ) : (
-          <div className="NTQuestionBlock">
-            <p className="NTQuestionBlock_TestName">{testName}</p>
-            <p className="NTQuestionBlock_Title">Вопросы:</p>
-            <div className="NTQuestionBlock_Items">
-              {listQuestion.length ? (
-                listQuestion.map(({ id, ordering, text }) => (
-                  <div
-                    key={`questionItem_${id}`}
-                    className="NTQuestionBlock_Items_Item"
-                  >
-                    <p>
-                      {ordering}. {text}
-                    </p>
-                    <div className="NT_IconBlock">
-                      <EditTwoTone
-                        onClick={() => id && openEditModal(id, text)}
-                      />
-                      <DeleteTwoTone onClick={() => id && deleteQuestion(id)} />
-                    </div>
-                  </div>
-                ))
-              ) : (
-                <p>В этом тесте ещё нет ни одного вопроса</p>
-              )}
-              <Button
-                className="NTButton_add"
-                type="primary"
-                shape="round"
-                icon={<PlusOutlined />}
-                size={"middle"}
-                onClick={openModal}
-              >
-                Добавить вопрос
-              </Button>
-            </div>
+          <div className="NTWrapper">
+            <p className="NT_TestName">{testName}</p>
+            <AddQuestion testId={testId} />
           </div>
         )}
       </div>
-      <Modal
-        visible={isModalVisible}
-        onOk={handleModalOK}
-        onCancel={handleModalCancel}
-      >
-        <AddQuestion
-          questionId={questionId}
-          testId={testId}
-          setQuestionId={setQuestionId}
-          listQuestion={listQuestion}
-        />
-      </Modal>
     </Main>
   );
 };
