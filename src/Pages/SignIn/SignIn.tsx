4bd1281fd56d9e4f0493ef1131e3bf4052b99bb3
vlzelensky <vzelensky.exceedteam@gmail.com>
2022-06-09 13:00:17 +0300

    feat/add prettier config

--- src/Pages/SignIn/SignIn.tsx
+++ src/Pages/SignIn/SignIn.tsx
@@ -1,13 +1,13 @@
-import "antd/dist/antd.css";
-import { Form, Input, Button } from "antd";
-import { SignInUp } from "../../types/types";
-import { Link, useNavigate } from "react-router-dom";
-import { AuthService } from "../../services/AuthService";
-import { LoginView } from "../../Layouts/LoginView/LoginView";
-import { UserService } from "../../services/UserService";
-import { userAtom } from "../../atoms/userAtom";
-import { useAtom } from "jotai";
-import { useEffect } from "react";
+import 'antd/dist/antd.css';
+import { Form, Input, Button } from 'antd';
+import { SignInUp } from '../../types/types';
+import { Link, useNavigate } from 'react-router-dom';
+import { AuthService } from '../../services/AuthService';
+import { LoginView } from '../../Layouts/LoginView/LoginView';
+import { UserService } from '../../services/UserService';
+import { userAtom } from '../../atoms/userAtom';
+import { useAtom } from 'jotai';
+import { useEffect } from 'react';
 
 export const SignIn = () => {
   const [, setUser] = useAtom(userAtom);
@@ -16,11 +16,11 @@ export const SignIn = () => {
     AuthService.signin(e)
       .then((r) => {
         if (r.status === 200) {
-          alert("Successful authorization");
+          alert('Successful authorization');
           UserService.getMe().then((r) => {
             let user = r.data;
             setUser(user);
-            navigate("/home");
+            navigate('/home');
           });
         }
       })
@@ -28,10 +28,10 @@ export const SignIn = () => {
         console.log(e.response);
         switch (e.response.status) {
           case 403:
-            alert("Unknown error");
+            alert('Unknown error');
             break;
           case 401:
-            alert("Unauthorized. Incorrect login or password");
+            alert('Unauthorized. Incorrect login or password');
             break;
         }
       });
@@ -43,66 +43,66 @@ export const SignIn = () => {
 
   return (
     <LoginView>
-      <div className="SITitle">
+      <div className='SITitle'>
         <h1>Авторизация</h1>
         <p>Введите свои данные, чтобы войти</p>
       </div>
-      <Form onFinish={onSubmit} layout="vertical">
+      <Form onFinish={onSubmit} layout='vertical'>
         <Form.Item
-          label="Имя пользователя"
-          name="username"
+          label='Имя пользователя'
+          name='username'
           hasFeedback
           rules={[
             {
               required: true,
-              message: "Введите имя пользователя",
+              message: 'Введите имя пользователя',
             },
             {
               whitespace: true,
-              message: "Не начинайте с пробелов",
+              message: 'Не начинайте с пробелов',
             },
             {
               pattern: /^[a-zA-Zа-яА-Я\s]+$/,
-              message: "Только русские или латинские буквы",
+              message: 'Только русские или латинские буквы',
             },
             {
               min: 2,
-              message: "Минимум 2 символа",
+              message: 'Минимум 2 символа',
             },
           ]}
         >
-          <Input placeholder="Имя пользователя" />
+          <Input placeholder='Имя пользователя' />
         </Form.Item>
         <Form.Item
-          label="Пароль"
-          name="password"
+          label='Пароль'
+          name='password'
           hasFeedback
           rules={[
             {
               required: true,
-              message: "Введите пароль",
+              message: 'Введите пароль',
             },
             {
               whitespace: true,
-              message: "Не начинайте с пробелов",
+              message: 'Не начинайте с пробелов',
             },
             {
               pattern: /^(?=.*\d)[a-zA-Z\d]{6,25}$/,
-              message: "Минимум 6 символов, латинские буквы и минимум 1 цифра",
+              message: 'Минимум 6 символов, латинские буквы и минимум 1 цифра',
             },
           ]}
         >
-          <Input.Password placeholder="Пароль" />
+          <Input.Password placeholder='Пароль' />
         </Form.Item>
         <Form.Item>
-          <Button block type="primary" htmlType="submit">
+          <Button block type='primary' htmlType='submit'>
             Войти
           </Button>
         </Form.Item>
       </Form>
-      <p className="SIDownTitle">
+      <p className='SIDownTitle'>
         Если ещё нет аккаунта, <br />
-        <Link to="/signup">зарегистрируйтесь</Link>
+        <Link to='/signup'>зарегистрируйтесь</Link>
       </p>
     </LoginView>
   );
