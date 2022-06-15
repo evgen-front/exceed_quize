33254494264f57d0ad5e5eb3dd48dd9360fce2de
vlzelensky <vzelensky.exceedteam@gmail.com>
2022-06-15 11:14:33 +0300

    fix PR according to comments

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -6,16 +6,20 @@ import { HOME, PROFILE } from '../../Router/routes';
 import { userAtom } from '../../atoms/userAtom';
 import { NavBarLink } from './modules/NavBarLink';
 
-const navLinks = [
+const navLinks: {
+  title: string;
+  to: string;
+  Icon: React.ElementType<{ size: number | string }>;
+}[] = [
   {
     title: 'Home',
     to: HOME,
-    icon: <MdHome size={33} />,
+    Icon: MdHome,
   },
   {
     title: 'Profile',
     to: PROFILE,
-    icon: <MdAccountCircle size={33} />,
+    Icon: MdAccountCircle,
   },
 ];
 
@@ -24,7 +28,7 @@ export const Navbar: React.FC = () => {
   const shouldDisplayNavbar = !!user;
 
   if (!shouldDisplayNavbar) {
-    return <></>;
+    return null;
   }
 
   return (
@@ -33,11 +37,11 @@ export const Navbar: React.FC = () => {
       justifyContent='space-around'
       alignItems='center'
       bg='white'
-      height=' 65px'
+      height='65px'
       borderRadius=' 15px 15px 0 0'
     >
-      {navLinks.map(({ title, to, icon }) => (
-        <NavBarLink key={title} title={title} to={to} icon={icon} />
+      {navLinks.map(({ title, to, Icon }) => (
+        <NavBarLink key={title} title={title} to={to} icon={<Icon size={33} />} />
       ))}
     </Box>
   );
